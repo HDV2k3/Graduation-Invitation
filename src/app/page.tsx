eb8cf5ad49affb3e3c491f8a8661a2ce1c58a2b5
Huynh Dac Viet <HDV2k3@users.noreply.github.com>
2025-11-23 23:45:23 +0700

    build project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { BannerSection } from "../components/BannerSection";
-import { DirectionsSection } from "../components/DirectionsSection";
 import { GraduateInfoSection } from "../components/GraduateInfoSection";
 import { EventInfoSection } from "../components/EventInfoSection";
 import { TimelineSection } from "../components/TimelineSection";
 import { ParkingSection } from "../components/ParkingSection";
 import { MapSection } from "../components/MapSection";
-import { ActionSection } from "../components/ActionSection";
 import { FooterSection } from "../components/FooterSection";
 
 export default function InvitationPage() {
